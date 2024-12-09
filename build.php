4a30ee9c8abbf8a70997179f57adeae62e51be87
Jack Harris <st_tuff@icloud.com>
2024-12-09 15:52:56 +1100

    Created build script to build our phars, this will be setup in github actions later to trigger on release.

---
+++ build.php
@@ -0,0 +1,29 @@
+<?php
+
+// build.php
+$pharFile = 'lucent.phar';
+
+// Clean up existing PHAR
+if (file_exists($pharFile)) {
+    unlink($pharFile);
+}
+
+// Create new PHAR
+$phar = new Phar($pharFile);
+
+// Start adding files
+$phar->buildFromDirectory(__DIR__.DIRECTORY_SEPARATOR."src", '/\.(php|env)$/');
+
+// Create custom stub
+$stub = <<<'EOF'
+<?php
+Phar::mapPhar();
+
+// Load the framework's autoloader and program
+require 'phar://' . __FILE__ . '/autoloader.php';
+
+__HALT_COMPILER();
+EOF;
+
+// Set the stub
+$phar->setStub($stub);
