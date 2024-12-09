0d0acc8f32750850c4b98939a06edf19759a8145
Jack Harris <st_tuff@icloud.com>
2024-12-09 16:01:25 +1100

    The DatabaseColumn attribute allows us to specify how we want a models variable to be transated into a MySQl column.

---
+++ src/Lucent/Database/Attributes/DatabaseColumn.php
@@ -0,0 +1,48 @@
+<?php
+namespace Lucent\Database\Attributes;
+
+use Attribute;
+
+#[Attribute]
+class DatabaseColumn
+{
+
+    public private(set) array $column;
+
+    public function __construct(array $properties)
+    {
+        //Define our column defaults
+        $this->column = [
+            "NAME" => null,
+            "ALLOW_NULL" => true,
+            "LENGTH" => 255,
+            "AUTO_INCREMENT" => false,
+            "TYPE" => null,
+            "PRIMARY_KEY" => false,
+            "DEFAULT" => null,
+            "VALUES" => [],
+            "UNIQUE_KEY_TO" => null,
+            "ON_UPDATE" => null,
+            "REFERENCES" => null,
+            "UNIQUE" => null
+        ];
+
+        //Loop over all our properties and translate them into our column
+        foreach (array_keys($this->column) as $item){
+            if(array_key_exists($item,$properties)){
+                $this->column[$item] = $properties[$item];
+            }
+        }
+    }
+
+    public function setName(string $name): void
+    {
+        $this->column["NAME"] = $name;
+    }
+
+    public function getColumn(): array
+    {
+        return $this->column;
+    }
+
+}
