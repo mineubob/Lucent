d64c95aed6ec3bcba8186813b3f35bdbb2c23e30
Jack Harris <st_tuff@icloud.com>
2025-02-14 11:33:49 +1100

    - Updated the Route facade to remove our old methods in favor of using the new rest group.

--- src/Lucent/Facades/Route.php
+++ src/Lucent/Facades/Route.php
@@ -9,42 +9,13 @@ namespace Lucent\Facades;
 
 
 use Lucent\Application;
-use Lucent\Http\HttpRouter;
+use Lucent\Routing\RestRouteBuilder;
 
 class Route
 {
 
-    public static function get(string $uri, string $method, string $controller): void
-    {
-        Application::getInstance()->getHttpRouter()->RegisterRoute($uri,HttpRouter::$ROUTE_GET,$method,$controller);
-    }
-
-    public static function post(string $uri, string $method, string $controller): void
-    {
-
-        Application::getInstance()->getHttpRouter()->RegisterRoute($uri,HttpRouter::$ROUTE_POST,$method,$controller);
-    }
-
-    public static function patch(string $uri, string $method, string $controller): void
-    {
-        Application::getInstance()->getHttpRouter()->RegisterRoute($uri,HttpRouter::$ROUTE_PATCH,$method,$controller);
-    }
-
-    public static function delete(string $uri, string $method, string $controller): void
-    {
-        Application::getInstance()->getHttpRouter()->RegisterRoute($uri,HttpRouter::$ROUTE_DELETE,$method,$controller);
-    }
-
-    public static function middleware(array $middleware,$callback): void
-    {
-        Application::getInstance()->getHttpRouter()->setActiveMiddleware($middleware);
-        $callback();
-        Application::getInstance()->getHttpRouter()->setActiveMiddleware([]);
-    }
-
-    public static function errorPage(int $code,string $view,array $middleware = []): void
-    {
-        Application::getInstance()->setErrorPage($code,$view,$middleware);
+    public static function rest() : RestRouteBuilder{
+        return new RestRouteBuilder(Application::getInstance()->getHttpRouter());
     }
 
 
