907590dd107da5e073ba271bcee4439affc1d0e2
Jack Harris <st_tuff@icloud.com>
2025-02-15 08:10:45 +1100

    - Removed extra logging steps.

--- src/Lucent/Http/HttpRouter.php
+++ src/Lucent/Http/HttpRouter.php
@@ -19,10 +19,6 @@ class HttpRouter extends Router
         // Build the full URI with prefix
         $fullUri = $prefix ? $prefix . '/' . $uri : $uri;
 
-        // Debug registration
-        Log::channel('phpunit')->info("Registering route: {$type} /{$fullUri}");
-        Log::channel('phpunit')->info("Controller: " . ($controller ?? 'default') . ", Method: {$method}");
-
         // Store the route without leading slash for consistent comparison
         $this->routes[$type][$fullUri] = [
             "controller" => $controller,
