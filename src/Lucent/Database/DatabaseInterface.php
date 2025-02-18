165001e56cb335ea94182a6205962aa80720fdd7
Jack Harris <st_tuff@icloud.com>
2025-02-18 15:31:36 +1100

    - Created a new DatabaseInterface, this is what all our database drivers will extend.

---
+++ src/Lucent/Database/DatabaseInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Lucent\Database;
+
+
+abstract class DatabaseInterface
+{
+
+    abstract public function query(string $query): bool|array;
+    abstract public function fetch(string $query): array;
+    abstract public function fetchAll(string $query): array;
+
+    abstract public function createTable(string $name, array $columns): string;
+
+    abstract public function getTypeMap() :array;
+}
