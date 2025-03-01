cb0db64a29890ad42246aafb07ea00d1d9095f45
Jack Harris <st_tuff@icloud.com>
2025-03-01 15:05:43 +1100

    - Added the allowed query calls to the database interface, this will prevent users from executing SQL via the wrong helpers. - Added the new validator this validates SQL before running it. - Removed old query, fetch and fetchAll methods. - Added statement, insert, delete,update,select,transation, hasTable, hasColumn and getAutoIncirmentId methods.

--- src/Lucent/Database/DatabaseInterface.php
+++ src/Lucent/Database/DatabaseInterface.php
@@ -6,11 +6,34 @@ namespace Lucent\Database;
 abstract class DatabaseInterface
 {
 
-    abstract public function query(string $query): bool|array;
-    abstract public function fetch(string $query): array;
-    abstract public function fetchAll(string $query): array;
+    public protected(set) array $allowed_statement_prefix;
+    public protected(set) array $allowed_delete_prefix;
+    public protected(set) array $allowed_insert_prefix;
+    public protected(set) array $allowed_update_prefix;
+    public protected(set) array $allowed_select_prefix;
+
+    protected DatabaseValidator $validator;
+
+    public function __construct(){
+        $this->validator = new DatabaseValidator($this);
+    }
+
     abstract public function createTable(string $name, array $columns): string;
     abstract public function getTypeMap() :array;
-    abstract public function tableExists(string $dbName): bool;
     abstract public function lastInsertId(): string|int;
+
+    //Query Execution function
+    abstract public function statement(string $query): bool;
+    abstract public function insert(string $query): bool;
+    abstract public function delete(string $query): bool;
+    abstract public function update(string $query): bool;
+    abstract public function select(string $query,bool $fetchAll = true): ?array;
+    abstract public function transaction(callable $callback): bool;
+
+    //Table functions
+    abstract public function hasTable(string $name): bool;
+    abstract public function hasColumn(string $table,string|array $column): bool;
+
+    abstract public function getAutoincrementId() : int;
+
 }
