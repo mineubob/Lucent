27a272108ef1c55f2c36792bd76098a6466b7cf9
Jack Harris <st_tuff@icloud.com>
2025-03-11 16:01:39 +1100

    - Added a new DynamicRule class and created its new test cases, dynamic rules validate data thats present only.

--- tests/Unit/RuleTest.php
+++ tests/Unit/RuleTest.php
@@ -31,9 +31,37 @@ class NumRule extends TestRule
     }
 }
 
+class DynamicRule extends Rule
+{
+
+    private array $keys;
+
+    public function __construct(array $keys){
+        $this->keys = $keys;
+    }
+
+    public function setup(): array
+    {
+        $rules = [
+            'first_name' => [
+                'min:2',
+                'max:10',
+            ],
+            'last_name' => [
+                'min:2',
+                'max:10',
+            ]
+        ];
+
+        return array_filter($rules, function (string $field) {
+            return array_key_exists($field, $this->keys);
+        }, ARRAY_FILTER_USE_KEY);
+    }
+}
+
 class RuleTest extends TestCase
 {
-    public function test_num_rule_is_valid()
+    public function test_num_rule_is_valid() : void
     {
         $rule = new NumRule();
 
@@ -42,12 +70,53 @@ class RuleTest extends TestCase
         $this->assertTrue($rule->validate_bool($test_data));
     }
 
-    public function test_num_rule_is_not_valid()
+    public function test_num_rule_is_not_valid() : void
     {
         $rule = new NumRule();
 
         $test_data = Faker::request()->failing(NumRule::class)->all();
 
         $this->assertFalse($rule->validate_bool($test_data));
+    }
+
+    public function test_dynamic_rule_passing() : void
+    {
+        $request = Faker::request();
+        $request->setInput("first_name", "Jack");
+        $request->setInput("last_name", "Harris");
+        $request->setInput("email", "notvalid.com");
+
+        $rule = new DynamicRule($request->all());
+
+        $this->assertTrue($request->validate($rule));
+        $this->assertEmpty($request->getValidationErrors());
+    }
+
+    public function test_dynamic_rule_failing() : void
+    {
+        $request = Faker::request();
+        $request->setInput("first_name", "Jack");
+        $request->setInput("last_name", 1);
+
+        $rule = new DynamicRule($request->all());
+        $this->assertFalse($request->validate($rule));
+        $this->assertEquals(1,count($request->getValidationErrors()));
+
+    }
+
+    public function test_dynamic_rule_only_validates_present_fields(): void
+    {
+        $request = Faker::request();
+        $request->setInput("first_name", "Jack");
+
+        // Create a rule that only validates first_name
+        $rule = new DynamicRule(["first_name" => "Jack"]);
+
+        // Add an invalid last_name that would fail if validated
+        $request->setInput("last_name", "A");  // Too short, would fail min:2
+
+        // Should pass because last_name isn't in the keys array and won't be validated
+        $this->assertTrue($request->validate($rule));
+        $this->assertEmpty($request->getValidationErrors());
     }
 }
