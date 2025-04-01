eff140fd3a12ca4f44e90c56b32e9c1f72c185b5
Jack Harris <st_tuff@icloud.com>
2025-04-01 11:16:57 +1100

    - Updated DocumentationController to use new FileSystem facade.

--- src/Lucent/Commandline/DocumentationController.php
+++ src/Lucent/Commandline/DocumentationController.php
@@ -4,7 +4,7 @@ namespace Lucent\Commandline;
 
 use Lucent\Facades\App;
 use Lucent\Facades\Faker;
-use Lucent\Facades\File;
+use Lucent\Facades\FileSystem;
 use Lucent\Facades\Log;
 use Lucent\Http\Attributes\ApiEndpoint;
 use Lucent\Http\Attributes\ApiResponse;
@@ -36,7 +36,7 @@ class DocumentationController
         );
 
         // Save to file
-        $outputPath = File::rootPath()."storage" .DIRECTORY_SEPARATOR. 'documentation' . DIRECTORY_SEPARATOR;
+        $outputPath = FileSystem::rootPath().DIRECTORY_SEPARATOR."storage" .DIRECTORY_SEPARATOR. 'documentation' . DIRECTORY_SEPARATOR;
         if (!file_exists($outputPath)) {
             mkdir($outputPath, 0755, true);
         }
