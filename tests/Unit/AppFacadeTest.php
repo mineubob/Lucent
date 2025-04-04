6740c707455e151b86559d5d6a86ac4f1f0b26b6
Jack Harris <st_tuff@icloud.com>
2025-04-04 11:11:29 +1100

    - Created test case to validate App::currentRoute() is working.

---
+++ tests/Unit/AppFacadeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Unit;
+
+use Lucent\Facades\App;
+use PHPUnit\Framework\TestCase;
+
+class AppFacadeTest extends TestCase
+{
+
+    public function test_get_url():void
+    {
+
+        // Set up server environment for testing
+        $_SERVER["REQUEST_METHOD"] = "GET";
+        $_SERVER["REQUEST_URI"] = "/test/four";
+
+        $output =  App::execute();
+        $url = App::currentRoute();
+
+        $this->assertEquals(["test","four"], $url);
+    }
+
+}
