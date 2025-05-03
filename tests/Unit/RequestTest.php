c819d1c070af3bf445a2c86f7f318974a50a0fa6
Jack Harris <st_tuff@icloud.com>
2025-05-03 18:30:31 +1000

    - Updated Request testing to match new snake case style. - Added new testing method for setting a header.

--- tests/Unit/RequestTest.php
+++ tests/Unit/RequestTest.php
@@ -19,7 +19,7 @@ class RequestTest extends TestCase
         $_SERVER = [];
     }
 
-    public function testBearerTokenExtraction()
+    public function test_bearer_token_extraction()
     {
         // Test valid bearer token
         $_SERVER['HTTP_AUTHORIZATION'] = 'Bearer abc123token';
@@ -42,7 +42,7 @@ class RequestTest extends TestCase
         $this->assertNull($request->bearerToken());
     }
 
-    public function testHeaderRetrieval()
+    public function test_header_retrieval()
     {
         // Test basic header retrieval
         $_SERVER['HTTP_CONTENT_TYPE'] = 'application/json';
@@ -60,7 +60,7 @@ class RequestTest extends TestCase
         $this->assertNull($request->header('nonexistent'));
     }
 
-    public function testAllHeadersRetrieval()
+    public function test_all_headers_retrieval()
     {
         $_SERVER['HTTP_CONTENT_TYPE'] = 'application/json';
         $_SERVER['HTTP_ACCEPT'] = 'application/json';
@@ -76,7 +76,7 @@ class RequestTest extends TestCase
         $this->assertEquals('application/json', $headers['Content-Type']);
     }
 
-    public function testJsonRequestDetection()
+    public function test_json_request_detection()
     {
         // Test JSON content type detection
         $_SERVER['CONTENT_TYPE'] = 'application/json';
@@ -94,7 +94,7 @@ class RequestTest extends TestCase
         $this->assertFalse($this->invokeMethod($request, 'isJsonRequest'));
     }
 
-    public function testHeaderNormalization()
+    public function test_header_normalization()
     {
         $_SERVER['HTTP_CONTENT_TYPE'] = 'application/json';
         $_SERVER['HTTP_X_CUSTOM_HEADER'] = 'value';
@@ -111,7 +111,7 @@ class RequestTest extends TestCase
         $this->assertArrayNotHasKey('X_CUSTOM_HEADER', $headers);
     }
 
-    public function testHeaderSanitization()
+    public function test_header_sanitization()
     {
         $_SERVER['HTTP_X_UNSAFE'] = "test\r\nX-Injected: malicious";
         $request = new Request();
@@ -122,6 +122,19 @@ class RequestTest extends TestCase
         $this->assertArrayHasKey('X-Unsafe', $headers);
         $this->assertEquals("test\r\nX-Injected: malicious", $headers['X-Unsafe']);
         $this->assertArrayNotHasKey('X-Injected', $headers);
+    }
+
+    public function test_header_set(): void
+    {
+        $_SERVER['HTTP_CONTENT_TYPE'] = 'text/html';
+
+        $request = new Request();
+
+        $this->assertEquals('text/html', $request->header('Content-Type'));
+
+        $request->setHeader('Content-Type', 'application/json');
+        $this->assertEquals('application/json', $request->header('Content-Type'));
+
     }
 
     public function test_route_info() : void
