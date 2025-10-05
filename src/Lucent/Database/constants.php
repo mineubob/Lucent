a082ca43abb055799ba1f78d5c983122a9ba4c5a
Jack Harris <st_tuff@icloud.com>
2025-10-05 11:49:26 +1100

    - Updated the constant values to now map directly to the new fluid table column methods. - Added a function constant for foreign key checks.

--- src/Lucent/Database/constants.php
+++ src/Lucent/Database/constants.php
@@ -1,28 +1,32 @@
 <?php
-const LUCENT_DB_BINARY = 1;
-const LUCENT_DB_TINYINT = 2;
-const LUCENT_DB_DECIMAL = 3;
-const LUCENT_DB_INT = 4;
-const LUCENT_DB_JSON = 5;
-const LUCENT_DB_TIMESTAMP = 6;
-const LUCENT_DB_ENUM = 7;
-const LUCENT_DB_DATE = 8;
-const LUCENT_DB_TEXT = 10;
-const LUCENT_DB_VARCHAR = 12;
+const LUCENT_DB_BINARY = "binary";
+const LUCENT_DB_TINYINT = "tinyint";
+const LUCENT_DB_DECIMAL ="decimal";
+const LUCENT_DB_INT = "int";
+const LUCENT_DB_JSON = "json";
+const LUCENT_DB_TIMESTAMP = "timestamp";
+const LUCENT_DB_ENUM = "enum";
+const LUCENT_DB_DATE = "date";
+const LUCENT_DB_TEXT = "text"
+;
+const LUCENT_DB_VARCHAR = "varchar";
 
 // Additional useful constants
 const LUCENT_DB_DEFAULT_CURRENT_TIMESTAMP = 'CURRENT_TIMESTAMP';
 
 // You might also want to add these for completeness
-const LUCENT_DB_FLOAT = 13;
-const LUCENT_DB_DOUBLE = 14;
-const LUCENT_DB_BOOLEAN = 15;
-const LUCENT_DB_CHAR = 16;
-const LUCENT_DB_LONGTEXT = 17;
-const LUCENT_DB_MEDIUMTEXT = 18;
+const LUCENT_DB_FLOAT = "float";
+const LUCENT_DB_DOUBLE = "double";
+const LUCENT_DB_BOOLEAN = "boolean";
+const LUCENT_DB_CHAR = "bar";
+const LUCENT_DB_LONGTEXT = "longtext";
+const LUCENT_DB_MEDIUMTEXT = "mediumtext";
 
 // NEW: Add BIGINT support
-const LUCENT_DB_BIGINT = 19;
+const LUCENT_DB_BIGINT = "bigint";
 
 // NEW: Add UNSIGNED modifier (can be combined with other types)
 const LUCENT_DB_UNSIGNED = 'UNSIGNED';
+
+//Functions
+const LUCENT_DB_FOREIGN_KEY_CHECKS = "foreign_key_checks";
