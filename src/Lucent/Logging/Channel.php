c66b1a18d26681eb6e54ffdbcb72afdf1cc3b602
Mineubob <mineubob@apollocloudservices.com>
2025-10-08 04:57:24 +0800

    Update SELECT to support JOIN highlighting.

--- src/Lucent/Logging/Channel.php
+++ src/Lucent/Logging/Channel.php
@@ -70,8 +70,8 @@ class Channel
             // SELECT <value>
             '/^SELECT\s+(?!.*\b(FROM|WHERE|GROUP|ORDER|HAVING|LIMIT|OFFSET|JOIN)\b)[^;]+$/is',
 
-            // SELECT ... FROM <table> [WHERE ...] [GROUP BY ...] [ORDER BY ...]
-            '/^SELECT\s+.+\s+FROM\s+\S+(?:\s+WHERE\s+.+)?(?:\s+GROUP\s+BY\s+.+)?(?:\s+ORDER\s+BY\s+.+)?$/is',
+            // SELECT ... FROM <table> [[INNER|LEFT|RIGHT|FULL] [OUTER] JOIN .. ON ..] [WHERE ...] [GROUP BY ...] [ORDER BY ...]
+            '/^SELECT\s+.+\s+FROM\s+\S+(?:\s+(?:INNER|LEFT|RIGHT|FULL)?(?:\s+OUTER)?\s+JOIN\s+\S+\s+ON\s+.+?)*(\s+WHERE\s+.+)?(\s+GROUP\s+BY\s+.+)?(\s+ORDER\s+BY\s+.+)?(\s+LIMIT\s+\d+)?(\s+OFFSET\s+\d+)?$/is',
 
             // INSERT INTO table (...) VALUES (...)
             '/^INSERT\s+INTO\s+\S+\s*\([^)]+\)\s+VALUES\s*\([^\)]*?\)$/is',
@@ -159,6 +159,7 @@ class Channel
             'RIGHT',
             'INNER',
             'OUTER',
+            'FULL',
             'ON',
             'AS',
             'AND',
