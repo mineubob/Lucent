139de2c55e4c29e32ef2cf2a56be7eed571f5d6b
Jack Harris <st_tuff@icloud.com>
2025-10-11 20:14:45 +1100

    - Moved ColumnType to its own file.

---
+++ src/Lucent/Model/ColumnType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lucent\Model;
+
+enum ColumnType: string
+{
+    case BINARY = "binary";
+    case TINYINT = "tinyint";
+    case DECIMAL = "decimal";
+    case INT = "int";
+    case JSON = "json";
+    case TIMESTAMP = "timestamp";
+    case ENUM = "enum";
+    case DATE = "date";
+    case TEXT = "text";
+    case VARCHAR = "varchar";
+    case FLOAT = "float";
+    case DOUBLE = "double";
+    case BOOLEAN = "boolean";
+    case CHAR = "bar";
+    case LONGTEXT = "longtext";
+    case MEDIUMTEXT = "mediumtext";
+    case BIGINT = "bigint";
+}
