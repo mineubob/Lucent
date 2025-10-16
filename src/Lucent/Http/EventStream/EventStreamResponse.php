f42555d1e81c88ca8f52a7889ff3ab1e1c2e8c30
Jack Harris <st_tuff@icloud.com>
2025-10-16 15:59:53 +1100

    - Created EventStreamResponse, this is returned to the user when we open a new event stream.

---
+++ src/Lucent/Http/EventStream/EventStreamResponse.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lucent\Http\EventStream;
+
+use Lucent\Http\HttpResponse;
+
+class EventStreamResponse extends HttpResponse
+{
+    private $callback;
+
+    public function __construct(callable $callback)
+    {
+        parent::__construct('', 200);
+
+        // SSE headers
+        $this->headers['Content-Type'] = 'text/event-stream';
+        $this->headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
+        $this->headers['X-Accel-Buffering'] = 'no';
+        $this->headers['Connection'] = 'keep-alive';
+
+        $this->callback = $callback;
+    }
+
+    public function render(): string
+    {
+        if ($this->callback) {
+            call_user_func($this->callback);
+        }
+
+        return '';
+    }
+}
